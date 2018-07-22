274517a5c53dcef8e20a7e6ecf190786333b4aa5
ysabelm <ysabelmonfort@gmail.com>
2018-07-22 15:10:07 +0200

    Fixes scope issues declaring variables before the specs

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -103,7 +103,7 @@ $(function () {
 
     /* TODO: Write a new test suite named "New Feed Selection" */
     describe('New Feed Selection', function () {
-
+        var feedInitial, feedFinal;
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
